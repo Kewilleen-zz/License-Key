3495927b25bbe1cabb391d7000fbdee612cef3fb
Kewilleen Gomes <kewilleen@outlook.com>
2018-12-27 20:10:22 -0200

    Changing settings

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
-define('database_host', 'localhost');
-define('database_user', 'root');
-define('database_pass', '');
-define('database_name', 'test');
+date_default_timezone_set('America/Sao_Paulo');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+/* Mysql configurations */
+define("mysql", array('host'=>'localhost','user'=>'root', 'pass'=>'', 'db'=>'test'));
