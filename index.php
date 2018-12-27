6508df6df38835e18c2a71b34b38a25d0abee9dd
Kewilleen Gomes <kewilleen@outlook.com>
2018-12-27 20:14:20 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 	header("Content-type: application/json; charset=utf-8");
 	if (!isset($_GET['ip'], $_GET['port'])) {
-		die('{"error":"IP/Port not found!"}');
+		die('{"error":"IP or Port not found!"}');
 	}
 	require 'data/license.class.php';
 	$l = new License();
 	if (!$l->hasLicense($_GET['ip'], $_GET['port'])) {
-		die('{"error":"IP/Port no has a license!"}');
+		die('{"error":"IP dont has a license!"}');
 	}
 	echo $l->getJSON();
 ?>
