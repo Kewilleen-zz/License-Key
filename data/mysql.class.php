11be0ff78664198b78ffaf07b6161d77e7fa95a0
Kewilleen Gomes <kewilleen@outlook.com>
2018-12-27 20:11:41 -0200

    Adapting the new settings

--- data/mysql.class.php
+++ data/mysql.class.php
@@ -9,7 +9,7 @@ class MySQL	{
 
 	function __construct()	{
 		try {
-			$this->pdo = new PDO('mysql:host='.database_host.';dbname='.database_name, database_user, database_pass);
+			$this->pdo = new PDO('mysql:host=' . mysql['host'] . ';dbname=' . mysql['db'], mysql['user'], mysql['pass']);
 			$this->pdo->query('CREATE TABLE IF NOT EXISTS `license` (`ip` VARCHAR(20) NOT NULL, `port` SMALLINT NOT NULL, `plugin` VARCHAR(60) NOT NULL, `server` VARCHAR(60) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8');
 		} catch(PDOException $e) {
 			die('Error in pdo: '.$e->getMessage());
