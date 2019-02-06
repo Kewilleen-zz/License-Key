310cdcdd09ab65a4eb29a4859fc2866ebff74d60
Kewilleen Gomes <kewilleen@outlook.com>
2019-02-06 18:32:48 -0200

    Update mysql.class.php

--- data/mysql.class.php
+++ data/mysql.class.php
@@ -9,8 +9,8 @@ class MySQL	{
 
 	function __construct()	{
 		try {
-			unserialize(mysql);
-			$this->pdo = new PDO('mysql:host=' . mysql['host'] . ';dbname=' . mysql['db'], mysql['user'], mysql['pass']);
+			$mysql = unserialize(mysql);
+			$this->pdo = new PDO('mysql:host=' . $mysql['host'] . ';dbname=' . $mysql['db'], $mysql['user'], $mysql['pass']);
 			$this->pdo->query('CREATE TABLE IF NOT EXISTS `license` (`ip` VARCHAR(20) NOT NULL, `port` SMALLINT NOT NULL, `plugin` VARCHAR(60) NOT NULL, `server` VARCHAR(60) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8');
 		} catch(PDOException $e) {
 			die('Error in pdo: '.$e->getMessage());
